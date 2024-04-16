044ce3fcfb9690b37d250acc6a6c646f011c5ef1
ryanf27 <ryanfebry0227@gmail.com>
2024-04-16 11:19:33 +0700

    create register route api

---
+++ src/app/api/register/route.js
@@ -0,0 +1,31 @@
+import User from "@/models/User";
+import connectToDB from "@/utils/db";
+import bcrypt from "bcrypt";
+import { NextResponse } from "next/server";
+
+export async function registerUser(request) {
+  try {
+    await connectToDB();
+    const userData = await request.json();
+    const { username, email, password } = userData;
+
+    const userExists = await User.findOne({ email }).lean().exec();
+    if (userExists) {
+      return NextResponse.json(
+        { message: "User already exists" },
+        { status: 409 }
+      );
+    }
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({
+      username,
+      email,
+      password: encryptedPassword,
+    });
+
+    return NextResponse.json({ user: newUser }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
