ed41895fc0eb22afe43ce5839932997a07b0ea91
ryanf27 <ryanfebry0227@gmail.com>
2024-04-22 16:50:41 +0700

    create profile page

--- src/app/api/profile/route.tsx
+++ src/app/api/profile/route.tsx
@@ -1,40 +1,98 @@
 import User from "@/models/User";
 import connectToDB from "@/utils/db";
-import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
-import { NextResponse } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
+import bcrypt from "bcrypt";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET() {
   try {
     await connectToDB();
 
     const session = await getServerSession();
 
     if (!session) {
-      return res.status(401).json({ message: "Unauthorized" });
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
     }
 
     const userEmail = session?.user?.email;
 
     if (!userEmail) {
-      return res.status(400).json({ message: "Invalid session data" });
+      return NextResponse.json(
+        { message: "Invalid session data" },
+        { status: 400 }
+      );
     }
     const userData = await User.findOne({ email: userEmail });
 
     if (!userData) {
-      return res.status(404).json({ message: "User not found" });
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
     }
 
-    // const sanitizedUser = {
-    //   id: userData._id,
-    //   username: userData.username,
-    //   email: userData.email,
-    //   password:userData.password
-    // };
-    console.log("session", userData);
-    return NextResponse.json({ userData, status: 200 });
+    return NextResponse.json({ userData }, { status: 200 });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Internal server error" });
+    NextResponse.json({ message: "Internal server error" }, { status: 500 });
+  }
+}
+
+export async function PUT(req: NextRequest) {
+  await connectToDB();
+
+  const session = await getServerSession();
+
+  if (!session) {
+    console.log("No session found, unauthorized access");
+    return new NextResponse(JSON.stringify({ message: "Unauthorized" }), {
+      status: 401,
+    });
+  }
+
+  try {
+    const { username, email, newPassword, oldPassword } = await req.json();
+
+    if (!username || !email) {
+      console.log("Required fields missing");
+      return new NextResponse(
+        JSON.stringify({ message: "Please fill in all required fields" }),
+        { status: 400 }
+      );
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      console.log("Profile not found for email:", email);
+      return new NextResponse(
+        JSON.stringify({ message: "Profile not found" }),
+        { status: 404 }
+      );
+    }
+
+    if (newPassword && oldPassword) {
+      if (!bcrypt.compareSync(oldPassword, existingUser.password)) {
+        console.log("Incorrect old password for user:", existingUser);
+        return new NextResponse(
+          JSON.stringify({ message: "Incorrect old password" }),
+          { status: 400 }
+        );
+      }
+      console.log("Password match, updating password for user:", existingUser);
+      existingUser.password = await bcrypt.hash(newPassword, 10);
+    }
+
+    existingUser.username = username;
+    existingUser.email = email;
+
+    await User.findOneAndUpdate({ email }, existingUser, { new: true });
+
+    return new NextResponse(
+      JSON.stringify({ message: "Profile updated successfully!" }),
+      { status: 200 }
+    );
+  } catch (err) {
+    console.error("Error during profile update:", err);
+    return new NextResponse(JSON.stringify({ message: "Server error" }), {
+      status: 500,
+    });
   }
 }
--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React, { useState, useEffect } from "react";
 import {
   Container,
@@ -7,50 +8,94 @@ import {
   TextField,
   Button,
   Avatar,
+  CircularProgress,
 } from "@mui/material";
 
 interface ProfileData {
   username: string;
   email: string;
-  password: string;
+  oldPassword: string;
+  newPassword: string;
+  confirmPassword: string;
 }
 
 const Profile: React.FC = () => {
-  const [profile, setProfile] = useState<ProfileData | null>(null);
+  const [profile, setProfile] = useState<ProfileData>({
+    username: "",
+    email: "",
+    oldPassword: "",
+    newPassword: "",
+    confirmPassword: "",
+  });
+  const [editProfile, setEditProfile] = useState<ProfileData>();
+
   const [isLoading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchProfile = async () => {
       setLoading(true);
-      setError(null);
-
       try {
         const response = await fetch("/api/profile");
-
         if (!response.ok) {
           throw new Error(`Failed to fetch profile data: ${response.status}`);
         }
-
         const data = await response.json();
-
         setProfile(data.userData);
+        setEditProfile(data.userData);
       } catch (err) {
         setError(err instanceof Error ? err.message : String(err));
       } finally {
         setLoading(false);
       }
     };
-
     fetchProfile();
   }, []);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setEditProfile((prev: any) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    setLoading(true);
+    setError(null);
+    if (profile.newPassword !== profile.confirmPassword) {
+      setError("New password and confirm password do not match.");
+      return;
+    }
+
+    try {
+      const response = await fetch("/api/profile", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(editProfile),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Failed to update profile: ${response.status}`);
+      }
+
+      const updatedData = await response.json();
+      setProfile(updatedData);
+      alert("Profile updated successfully!");
+    } catch (err) {
+      setError(err instanceof Error ? err.message : String(err));
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <Container component="main" maxWidth="md" sx={{ mt: 8 }}>
       {isLoading ? (
-        <Typography variant="body1" color="textSecondary" align="center">
-          Loading profile...
-        </Typography>
+        <CircularProgress />
       ) : error ? (
         <Typography variant="body1" color="error" align="center">
           Error: {error}
@@ -65,32 +110,66 @@ const Profile: React.FC = () => {
             alt="Profile Picture"
             sx={{ width: 90, height: 90, mx: "auto" }}
           />
-          <form noValidate autoComplete="off">
+          <form noValidate autoComplete="off" onSubmit={handleSubmit}>
             <TextField
               fullWidth
               label="Name"
               margin="normal"
-              value={profile.username}
+              name="username"
+              value={editProfile?.username || ""}
               variant="outlined"
-              disabled
+              onChange={handleChange}
             />
             <TextField
               fullWidth
               label="Email"
               margin="normal"
-              value={profile.email}
+              name="email"
+              value={editProfile?.email || ""}
               variant="outlined"
-              disabled
+              onChange={handleChange}
+            />
+
+            <Typography variant="h6" sx={{ mt: 4 }}>
+              Change Password
+            </Typography>
+
+            <TextField
+              fullWidth
+              label="Old Password"
+              type="password"
+              margin="normal"
+              name="oldPassword"
+              value={profile.oldPassword}
+              variant="outlined"
+              onChange={handleChange}
             />
             <TextField
               fullWidth
-              label="Password"
+              label="New Password"
+              type="password"
               margin="normal"
-              value={profile.password}
+              name="newPassword"
+              value={profile.newPassword}
               variant="outlined"
-              disabled
+              onChange={handleChange}
             />
-            <Button variant="contained" color="primary" sx={{ mt: 2 }}>
+            <TextField
+              fullWidth
+              label="Confirm New Password"
+              type="password"
+              margin="normal"
+              name="confirmPassword"
+              value={profile.confirmPassword}
+              variant="outlined"
+              onChange={handleChange}
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              sx={{ mt: 2 }}
+            >
               Update Profile
             </Button>
           </form>
